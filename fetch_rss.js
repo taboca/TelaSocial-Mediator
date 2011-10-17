bee2125481fb54ec7c84e845a9d7708fd939a4e6
Marcio Galli <taboca@gmail.com>
2011-10-17 19:58:26 -0200

    new fetch code attempts to fetch entire file to the disk

--- fetch_rss.js
+++ fetch_rss.js
@@ -6,28 +6,26 @@ var sys = require("sys"),
     rss = require('./3rdparty/node-rss/node-rss');
  
 function ruleLoadSaveRSS(name, url) {
-        var feed_url = 'http://www.latinoware.org/en/rss.xml';
-        var feed_url = url;
-        var buffer = "";
-        var response = rss.parseURL(feed_url, function(itemArticle) {
-            sys.puts("Got article and items are " + itemArticle.length);
 
-                // This is a hack — we keep an RSS but we will need a lot of 
-                // code here to allow mediation with filter ie pipe style 
-                // chain of events? 
-                buffer += '<?xml version="1.0" encoding="utf-8"?><rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/"> <channel><title>proxy social</title>';
-            for(i=0; i<itemArticle.length; i++) {
-                buffer+='<item><title><![CDATA['+itemArticle[i].title+']]></title>';
-                buffer+='<link>'+itemArticle[i].link+'</link>';
-                buffer+='<description><![CDATA['+itemArticle[i].description+']]></description>';
-                buffer+='<pubDate><![CDATA['+itemArticle[i].pubDate+']]></pubDate></item>';
-            }
-            buffer+= '</channel></rss>';
-            fs.writeFile('channel/'+name+'.xml', buffer, 'utf-8', function(err){
-                if (err) throw err
-                console.log('File saved.')
-            })
-        });
+	var buffer = "";
+	var host = url.parse(href).host;
+	var path = url.parse(href).path;
+        var options = {
+            host: host,
+            port: 80,
+            path: path
+        };
+        var request = http.get(options);
+
+        request.on('response', function (res) {
+            res.on('data', function (buffer) {
+       	       fs.writeFile('channel/'+name+'.xml', buffer, 'utf8', function(err){
+       	        if (err) throw err
+       	        console.log('File saved.')
+               });
+           });
+        })
+
 }
 
 sys.puts("trying to open "+ process.argv[3]);
